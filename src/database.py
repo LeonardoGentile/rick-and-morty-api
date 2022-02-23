0b018dfbc505ebb271e3e78b32d35934dc865dcb
Leonardo Gentile <leonard.gentile@gmail.com>
2022-02-23 21:02:30 +0100

    Plain file structure

---
+++ src/database.py
@@ -0,0 +1,31 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from src.settings import DATABASE_URL
+
+# specific for sqlite, ensuring you don't share the same
+# session with more than one request, the code is already safe
+# because of https://fastapi.tiangolo.com/async/
+connect_args = {"check_same_thread": False}
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=True,
+    connect_args=connect_args
+)
+
+# Each instance of the SessionLocal class will be a database session
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+# create db and tables
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
